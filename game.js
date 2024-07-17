6e109556761a838435645925df4949e8f9134c40
I-Alex06 <96075512+I-Alex06@users.noreply.github.com>
2024-07-17 18:44:49 +0300

    Update game.js, game.html
    Locatii salvate in game.js(nu mai folosesc locatii.json), nu mai ia in considerare click-uri in 12.5%(1/8) din parti si 10% din inaltime, pauza de 1 sec intre clickuri

--- game.js
+++ game.js
@@ -1,6 +1,110 @@
 
-import data from './locatii.json' with {type: 'json' };
-console.log(data);
+// import data from './locatii.json' with {type: 'json' };
+// console.log(data);
+let data=[
+    "CATEDRALA",
+    "KARTING",
+    "SPITAL",
+    "TERASA",
+    "NAVA SPATIALA",
+    "PARC DE AVENTURA",
+    "PARCARE",
+    "TEATRU",
+    "SALON DE INFRUMUSETARE",
+    "SUBMARIN",
+    "PLANETARIU",
+    "HOTEL",
+    "SERA",
+    "GRADINA ZOOLOGICA",
+    "CABINET STOMATOLOGIC",
+    "METROU",
+    "PATINOAR",
+    "CIMITIR",
+    "PLAJA",
+    "DEPOZIT",
+    "TARG",
+    "GRADINA BOTANICA",
+    "NAVA DE CROAZIERA",
+    "CABINET DE OFTALMOLOGIE",
+    "CENTRU COMERCIAL",
+    "SUPERMARKET",
+    "MALL",
+    "STATIE DE TRAMVAI",
+    "RESTAURANT",
+    "GARA DE TREN",
+    "BALON CU AER CALD",
+    "AEROPORT",
+    "SALON DE COAFURA",
+    "BIBLIOTECA",
+    "PENSIUNE",
+    "VULCANIZARE",
+    "CINEMA",
+    "FABRICA",
+    "BIROU",
+    "VAPOR DE CROAZIERA",
+    "CENTRU DE ESCALADA",
+    "CAMIN STUDENTESC",
+    "OBSERVATOR",
+    "UNIVERSITATE",
+    "SECTIE DE POLITIE",
+    "TELECABINA",
+    "SANTIER DE CONSTRUCTII",
+    "PIATA",
+    "PAINTBALL",
+    "SCOALA",
+    "FERMA",
+    "BAR",
+    "TEREN DE TENIS",
+    "GARA DE AUTOBUZ",
+    "SALON DE TATUAJE",
+    "CURTEA DE JUDECATA",
+    "MUZEU",
+    "CASTEL MEDIEVAL",
+    "SALA DE FITNESS",
+    "CABINET VETERINAR",
+    "PARC",
+    "PORT",
+    "FARMACIE",
+    "GARA",
+    "PARC DE TRAMBULINE",
+    "LABORATOR DE CHIMIE",
+    "STADION",
+    "CABINET MEDICAL",
+    "CLUB",
+    "LICEU",
+    "ATELIER AUTO",
+    "MAGAZIN",
+    "TAXI",
+    "SPA",
+    "AQUARIUM",
+    "SALA DE SPECTACOLE",
+    "REFUGIU ALPIN",
+    "SALA DE SPORT",
+    "GARAJ",
+    "BANCA",
+    "RULOTA",
+    "GRADINA",
+    "CIRC",
+    "GRADINITA",
+    "PADURE",
+    "CAZINOU",
+    "TRIBUNAL",
+    "PARC DE DISTRACTII",
+    "GALERIE DE ARTA",
+    "TEMPLU",
+    "TOALETA",
+    "AMBASADA",
+    "BISERICA",
+    "CABINET GINECOLOGIC",
+    "CAFENEA",
+    "CLADIRE DE BIROURI",
+    "PISCINA",
+    "LIBRARIE",
+    "PARC DE SKATEBOARD",
+    "PESTERA",
+    "INSULA TROPICALA"
+    ]
+    
 // data=[
 //     "aeroport",
 //     "toalete",
@@ -28,7 +132,13 @@ window.onload = function(){
     loc=data[i].toUpperCase();
     
 }
-addEventListener("click",function(){
+var notReady=false;
+addEventListener("click",function(e){
+    let sw=window.innerWidth;
+    let sh=window.innerHeight;
+    let cx=e.clientX;
+    let cy=e.clientY;
+    if(cy<sh/10 ||cy>sh/10*9 || cx<sw/8 ||cx>sw/8*7||notReady){return;}
     let p=document.getElementById("player");
     let w=document.getElementById("word");
     if(parseInt(p.innerHTML)>=nop && w.innerHTML!=""){
@@ -48,4 +158,6 @@ addEventListener("click",function(){
             p.innerHTML=(parseInt(p.innerHTML)+1);
         }
     }
+    notReady=true;
+    this.setTimeout(function(){notReady=false;},1000)
 })
