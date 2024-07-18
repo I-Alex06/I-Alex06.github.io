ae940d1e337988e89512e61bcaf20339804cfbe2
I-Alex06 <96075512+I-Alex06@users.noreply.github.com>
2024-07-18 22:18:13 +0300

    Update game.js

--- game.js
+++ game.js
@@ -142,7 +142,7 @@ addEventListener("click",function(e){
     let p=document.getElementById("player");
     let w=document.getElementById("word");
     if(parseInt(p.innerHTML)>=nop && w.innerHTML!=""){
-        p.innerHTML="1";
+        //p.innerHTML="1";
         window.location.href="timer.html";
     }
     else{
@@ -171,7 +171,7 @@ addEventListener('touchstart',function(e){
     let p=document.getElementById("player");
     let w=document.getElementById("word");
     if(parseInt(p.innerHTML)>=nop && w.innerHTML!=""){
-        p.innerHTML="1";
+        //p.innerHTML="1";
         window.location.href="timer.html";
     }
     else{
